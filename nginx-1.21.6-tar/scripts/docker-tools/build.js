58257a64938fc2742ace943787c7bd9e6b873855
luqiangzeng <luqiangzeng@tencent.com>
2022-02-12 17:15:38 +0800

    feat: 制作Nginx免安装包镜像

---
+++ nginx-1.21.6-tar/scripts/docker-tools/build.js
@@ -0,0 +1,33 @@
+'use strict';
+/**
+ *  构建 Nginx 镜像
+ *    npm run docker:build
+ */
+const path = require('path');
+const fs = require('fs');
+const { execSync } = require('child_process');
+
+const appDirectory = fs.realpathSync(process.cwd());
+const resolveApp = relativePath => path.resolve(appDirectory, relativePath);
+
+const APP_NAME = 'nginx-demo';
+const execOpts = { stdio: 'inherit' };
+
+const handleTnginxPackage = () => {
+  const nginxConfigPath = resolveApp('./packages/nginx-1.21.6/conf/nginx.conf');
+
+  // 解压 nginx-1.21.6.tar.gz
+  if (!fs.existsSync(nginxConfigPath)) {
+    execSync('mkdir -p packages', execOpts);
+    execSync(`tar -zxvf ${resolveApp('./packages/nginx-1.21.6.tar.gz')} -C ${resolveApp('packages')}`, execOpts);
+  }
+  // 替换 nginx.conf 配置文件
+  execSync(`cp -f ${resolveApp('./nginxConfig/nginx.conf')} ${nginxConfigPath}`);
+}
+handleTnginxPackage();
+
+console.info('构建镜像:');
+execSync(
+  `docker build -t ${APP_NAME} .`,
+  { stdio: 'inherit' },
+);
